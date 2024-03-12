18df7c4d2d0821ce6d66f872a4cdd11e9692009f
DegtevVictor <victorgt941@gmail.com>
2024-03-13 01:29:11 +0300

    add files

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(theRealMain);
+            thread.Start();
+        }
+
+        static void theRealMain()
+        {
+            for (int i = 5; i > 0; --i)
+            {
+                Console.WriteLine(DateTime.Now.ToLongTimeString());
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("done.");
+        }
+    }
+}
